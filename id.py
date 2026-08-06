0b948b72e40ead1de03d17adf71e2b04b598581a
DEVASHISH KHEDKAR <devashishkhedkar1234rk@gmail.com>
2026-08-06 16:19:50 +0530

    First Commit

---
+++ id.py
@@ -0,0 +1,11 @@
+v1 = 800
+v2 = 900
+v3 = 100
+
+print("Value of v1 :", v1)
+print("Value of v2 :", v2)
+print("Value of v3 :", v3)
+
+print("Memory Address of v1 :", id(v1))
+print("Memory Address of v2 :", id(v2))
+print("Memory Address of v3 :", id(v3))
---
+++ type.py
@@ -0,0 +1,13 @@
+a = 70
+b = 100.6
+c = " Five"
+d = True
+e = 2 + 3j
+f=10.0
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))    
+print(type(f))          
